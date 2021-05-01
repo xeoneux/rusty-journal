4bbd0439d3682d94b88ae6aa3710c18d069b6a04
Aayush Kapoor <xeoneux@icloud.com>
2021-05-01 05:16:49 -0400

    Read the file and add task

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,5 +1,6 @@
 use chrono::{serde::ts_seconds, DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use std::{fs::OpenOptions, io::{Seek, SeekFrom}, path::PathBuf};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
@@ -14,4 +15,29 @@ impl Task {
         let created_at = Utc::now();
         Task { text, created_at }
     }
+
+    pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+        let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(journal_path?)?;
+
+        let mut tasks = match serde_json::from_reader(file) {
+            Ok(tasks) => tasks,
+            Err(e) if e.is_eof() => Vec::new(),
+            Err(e) => Err(e)?,
+        };
+
+        file.seek(SeekFrom::Start(0))?;
+
+        tasks.push(task);
+        serde_json::to_writer(file, &tasks)?;
+
+        Ok(())
+    }
+
+    pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {}
+
+    pub fn list_tasks(journal_path: PathBuf) -> Result<()> {}
 }
